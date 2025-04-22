c689b2b122ece99da48cf5ef4a1e1c06712277ce
Kaue de Almeida <kaualmeidapena@gmail.com>
2025-04-22 09:25:08 -0300

    Adicionado programas com if, else, for, while e do while

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+// // Criar um programa que verificar a idade para votar
+
+// function verificarVoto(idade){
+//     if(idade>=16){
+//         console.log("Pode Votar!")
+//     }
+//     else{
+//         console.log("Não pode Votar!")
+//     }
+// }
+// verificarVoto(15)
+// verificarVoto(18)
+
+// //Crie um programa que imprime seus elementos em ARRAY(FOR)
+
+// const  codigos = ["codigo1", "codigo2", "codigo3", "codigo4"];
+
+// for(let i=0; i<codigos.length; i++){
+//     console.log("O valor de codigos: ", codigos[i]);
+// }
+
+// const linguagemProgramacao = ["java", "python", "C#", "js"];
+
+// for(let i=0; i<linguagemProgramacao.length; i++){
+//     console.log("A linguagem da programação: ", linguagemProgramacao[i])
+// }
+
+// //Crie um programa que valide a entrada do usuario(do while)
+// let numero;
+// do{
+//     numero = parseInt(prompt("Digite um número maior que 10:"))
+// }
+// while(numero<=9){
+//     console.log(`Número Válido ${numero}`)
+// }
+
+// //Crie um programa que represente um jogo de advinhação
+
+// let palpite;
+// const sorteio = Math.floor(Math.random() * 10)+1;
+// do{
+//     palpite = parseInt(prompt("Escolha um número de 1 e 10"))
+//     if(palpite !== sorteio){
+//     alert(`Você errou e perdeu 1000 reais!`)
+//     }
+// }
+// while(palpite!=sorteio){
+//     alert(`Você achou o Número! ${palpite}`)
+// }
+
+
